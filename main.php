c986a44a28c4f67801ba813ec98fc56a3cd7d4c4
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-19 19:23:00 +0100

    remove useless argv

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 use PierreMiniggio\YoutubeChannelCloner\App;
 
 try {
-    return (new App($argv))->run();
+    return (new App())->run();
 } catch (Exception $e) {
     echo get_class($e) . ' : ' . $e->getMessage();
 }
